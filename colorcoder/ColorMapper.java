4440227934eb81dd29155f8e6acaf7f503306821
hitheshpoojary <67824540+hitheshpoojary@users.noreply.github.com>
2025-08-13 18:22:26 +0530

    Create ColorMapper.java

---
+++ colorcoder/ColorMapper.java
@@ -0,0 +1,19 @@
+package colorcoder;
+
+public final class ColorMapper {
+    private ColorMapper() {}
+
+    public static final int NUMBER_OF_MAJOR_COLORS = MajorColor.values().length;
+    public static final int NUMBER_OF_MINOR_COLORS = MinorColor.values().length;
+
+    public static ColorPair getColorFromPairNumber(int pairNumber) {
+        int zeroBasedPairNumber = pairNumber - 1;
+        MajorColor majorColor = MajorColor.fromIndex(zeroBasedPairNumber / NUMBER_OF_MINOR_COLORS);
+        MinorColor minorColor = MinorColor.fromIndex(zeroBasedPairNumber % NUMBER_OF_MINOR_COLORS);
+        return new ColorPair(majorColor, minorColor);
+    }
+
+    public static int getPairNumberFromColor(MajorColor major, MinorColor minor) {
+        return major.getIndex() * NUMBER_OF_MINOR_COLORS + minor.getIndex() + 1;
+    }
+}
